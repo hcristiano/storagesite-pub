89ba3b06b4a3517365825f5929506996567ba9e6
hcristiano <40251016+hcristiano@users.noreply.github.com>
2023-05-17 16:57:34 -0800

    Initial Commit

---
+++ src/components/DownloadSection.vue
@@ -0,0 +1,68 @@
+<template>
+    <section id="discounts">
+        <v-overlay absolute>
+            <v-container fluid>
+                <v-row align="center" justify="center">
+                    <v-col cols="10">
+                        <v-row align="center" justify="center">
+                        
+                            <v-col cols="12" sm="10" class="white--text text-left">
+                                <h1 class="font-weight-light display-2 mb-2">{{title}}</h1>
+                                <h1 class="font-weight-light mb-3">
+                                    At Storage Times we offer:
+                                </h1>
+                                <v-row>
+                                    <v-col cols="12" 
+                                        class="d-flex align-center"
+                                        v-for="(deal, i) in deals"
+                                        :key="i">
+                                    
+                                        <p class="text-justify mb-0">
+                                            {{ deal }}
+                                        </p>
+                                    </v-col>
+                                </v-row>
+                                <v-btn v-bind="size" rounded outlined color="white" class="mt-6" @click="$vuetify.goTo('#contact')">
+                                    Request a quote today!
+                                </v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-overlay>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "Pricing",
+    data() {
+        return {
+            title: "Ask us about our Special Pricing and Discounts",
+            deals: [
+                "SPECIAL PRICING with one year rental including OneMonth Free.",
+                "Special Discounts for Military, Veterans, & First Responders"
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+#discounts {
+  background-image: url("~@/assets/img/storage_discount.png");
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  height: 500px;
+}
+#discounts .v-overlay__scrim{
+    opacity: .9!important;
+}
+#discounts .container,
+#discounts .row {
+  height: 100%;
+}
+</style>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+import '@mdi/font/css/materialdesignicons.css'
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: "#119DA4",
+                secondary: "#171b34",
+                accent: "3D87E4",
+                title: "#B71C1C",
+                contact: "#D32F2F",
+                dark: "#263238"
+            }
+        }
+    }
+});
